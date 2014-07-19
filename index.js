5a46fcc836e098eeb37945882bb783efd6df6211
Maximiliano Fierro <elmasse@gmail.com>
2014-07-19 13:48:50 -0300

    Fixing spaces & tabs.

--- index.js
+++ index.js
@@ -2,97 +2,97 @@
 'use strict';
 
 var cocktail     = require('cocktail'),
-	Configurable = require('cocktail-trait-configurable'),
-	Advisable    = require('./lib/Advisable');
+    Configurable = require('cocktail-trait-configurable'),
+    Advisable    = require('./lib/Advisable');
 
 var MyClass,
- 	obj;
+    obj;
 
 MyClass = cocktail.mix({
-	'@as': 'class',
+    '@as': 'class',
 
-	'@traits' : [Advisable, Configurable],
+    '@traits' : [Advisable, Configurable],
 
-	'@static' : {
-		create: function (options) {
-			return new this(options);
-		}
-	},
+    '@static' : {
+        create: function (options) {
+            return new this(options);
+        }
+    },
 
-	constructor: function (options) {
-		this.configure(options);
-		this.initialize();
-	},
+    constructor: function (options) {
+        this.configure(options);
+        this.initialize();
+    },
 
 
-	setAfter: function (options) {
-		var k,v;
-		for (k in options){
-			v = options[k];
-			this.after(k, v);
-		}
-	},
+    setAfter: function (options) {
+        var k,v;
+        for (k in options){
+            v = options[k];
+            this.after(k, v);
+        }
+    },
 
-	setBefore: function (options) {
-		var k,v;
-		for (k in options){
-			v = options[k];
-			this.before(k, v);
-		}
-	},
+    setBefore: function (options) {
+        var k,v;
+        for (k in options){
+            v = options[k];
+            this.before(k, v);
+        }
+    },
 
-	setAround: function (options) {
-		var k,v;
-		for (k in options){
-			v = options[k];
-			this.around(k, v);
-		}
-	},
+    setAround: function (options) {
+        var k,v;
+        for (k in options){
+            v = options[k];
+            this.around(k, v);
+        }
+    },
 
-	initialize: function () {
-		console.log('initialize');
-	},
+    initialize: function () {
+        console.log('initialize');
+    },
 
-	doSomething: function (withParams) {
-		console.log('do something with: ' + (withParams ? withParams : 'no parameters'));
-	},
+    doSomething: function (withParams) {
+        console.log('do something with: ' + (withParams ? withParams : 'no parameters'));
+    },
 
-	echo: function (param) {
-		return param;
-	},
+    echo: function (param) {
+        return param;
+    },
 
-	hmm: function(params) {
-		console.log( this.echo(params) );
-	}
+    hmm: function(params) {
+        console.log( this.echo(params) );
+    }
 });
 
 
 obj = MyClass.create({
-	before: {
-		'echo': function(param) {
-			if (param instanceof Object) {
-				param.modifiedValue = JSON.stringify(param);
-			}
-		}
-	},
-	after: {
-		'initialize': function(){
-			console.log('right after init')
-		},
-		'doSomething': function (withParams) {
-			console.log('After doSomething!');
-		}
-	}
+    before: {
+        'echo': function(param) {
+            if (param instanceof Object) {
+                param.modifiedValue = JSON.stringify(param);
+            }
+        }
+    },
+    after: {
+        'initialize': function(){
+            console.log('right after init')
+        },
+        'doSomething': function (withParams) {
+            console.log('After doSomething!');
+        }
+    }
 });
 
 
 // --- BEFORE
 
 obj.before(
-	'doSomething',
-	function(withParams) {
-		 console.log('Calling with Params: '+ (withParams ? 'YES' : 'NO'));
-	}
+    'doSomething',
+    function(withParams) {
+         console.log('Calling with Params: '+ (withParams ? 'YES' : 'NO'));
+    }
 );
 
 obj.doSomething();
@@ -106,9 +106,9 @@ console.log(obj.echo({a: 'value'}));
 // --- AROUND
 
 obj.around('hmm', function(method, params) {
-	if (typeof params[0] === 'string'){
-		method.apply(this, params);
-	}
+    if (typeof params[0] === 'string'){
+        method.apply(this, params);
+    }
 
 });
 
